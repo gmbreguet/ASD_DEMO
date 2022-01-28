31c6fc641d4042d85ac53d651df8769dcb3d9bab
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-01-28 08:34:47 +0100

    update GMB

--- 02_Recursivite/02_Hanoi.cpp
+++ 02_Recursivite/02_Hanoi.cpp
@@ -18,6 +18,8 @@ using Disque = char;
 using Tour   = vector<Disque>;
 using Tours  = vector<Tour>;
 
+size_t nbreAppels;      // variable globale pour compter les appels
+
 //------------------------------------------------------
 // afficher le contenu d'une tour (les disques)
 ostream& operator<< (ostream& os, const Tour& tour) {
@@ -36,7 +38,9 @@ ostream& operator<< (ostream& os, const Tours& tours) {
 }
 
 //------------------------------------------------------
-void transfert(Tour& from, Tour& via, Tour& to, int n) {
+void transfert(Tour& from, Tour& via, Tour& to, size_t n) {
+
+   ++nbreAppels;
 
    if (n > 0) {
       transfert(from, to, via, n-1);
@@ -62,11 +66,21 @@ int main () {
 
    cout << "Tour de Hanoi" << endl;
 
-   vector<Tour> tours(3);
-   tours[0] = {'A', 'B', 'C'};
+   vector<Tour> tours(3);                    // 3 tours
+   Tour& gauche = tours[0];
+   Tour& centre = tours[1];
+   Tour& droite = tours[2];
 
-   transfert(tours[0], tours[1], tours[2], 3);
+   // tour à déplacer
+   gauche = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+
+   //        from    via     to      height
+   transfert(gauche, centre, droite, gauche.size());
+
    cout << tours << endl;
+
+   cout << "hauteur     : " << droite.size() << endl;
+   cout << "nbre appels : " << nbreAppels    << endl;
 
    return EXIT_SUCCESS;
 }
