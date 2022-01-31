04743fc82ecbaa6ea0bf988afc2db9578f4ef53c
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-01-31 11:45:37 +0100

    update GMB

--- 01_Intro/01_somme_n.cpp
+++ 01_Intro/01_somme_n.cpp
@@ -2,7 +2,8 @@
 // Fichier        : 01_somme_n.cpp
 // Version        : 01 - 2021-12-05
 // Auteur(s)      : BREGUET Guy-Michel
-// But            : comparer les algorithmes de la somme des n premiers entier
+// But            : comparer les algorithmes de la somme
+//                  des n premiers entier
 //                   - de manière itérative
 //                   - algo 
 // Modifications  :
@@ -15,8 +16,8 @@
 using namespace std;
 
 //------------------------------------------------------
-int sommeN_iteratif(int n);
-int sommeN_Euclide (int n);
+int sommeN_iter(int n);
+int sommeN_Euclide(int n);
 
 //------------------------------------------------------
 // main
@@ -24,14 +25,14 @@ int sommeN_Euclide (int n);
 int main () {
 
    int n = 12;
-   cout << "boucle (" << n << ") = " << sommeN_iteratif(n)  << endl;
+   cout << "boucle (" << n << ") = " << sommeN_iter(n)      << endl;
    cout << "Euclide(" << n << ") = " << sommeN_Euclide(n)   << endl;
 
    return EXIT_SUCCESS;
 }  // main
 
 //------------------------------------------------------
-int sommeN_iteratif(int n) {
+int sommeN_iter(int n) {
    int s = 0;
    for (int i=0; i<=n; ++i)
       s += i;
