99f8a0bec3a617007f48ff539546b6effe346ea8
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-03-01 12:57:19 +0100

    update GMB

---
+++ 01_Intro/01_complexite_alternative.cpp
@@ -0,0 +1,43 @@
+//---------------------------------------------------------
+// Fichier        : 01_complexite_alternative.cpp
+// Version        : 01 - 2022-03-01
+// Auteur(s)      : BREGUET Guy-Michel
+// But            : Etude d'unbe conplexité altrnative
+//                  avec des moyennes pondérées
+// Modifications  :
+// Remarque(s)    : NILL
+// Application    :
+//---------------------------------------------------------
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+//------------------------------------------------------
+// main
+//------------------------------------------------------
+int main () {
+
+   int N = 10'000;
+   int k = 0;
+
+   for (int i=0; i<N; ++i) {
+      if (i%2) {
+         ++k;  // O(1)
+      }  // 0.5 * O(1)
+      else {
+         for (int j=N; j>0; j/=2) {
+            ++k;  // O(1)
+         }  // O(log(N))
+      }  // 0.5 * O(log(N))
+
+      // 0.5 * O(1) + 0.5 * O(log(N))
+      // 0.5 * (O(1) + O(log(N))
+      // => O(log(N)
+
+   }  // N * O(log(N) => O (N * LOG(N))
+
+
+   return EXIT_SUCCESS;
+}  // main
