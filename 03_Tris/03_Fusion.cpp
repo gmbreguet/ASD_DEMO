fce474060e1ff5f14f6fccc2d48623e47adda89e
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-03-17 15:00:18 +0100

    update GMB

--- 03_Tris/03_Fusion.cpp
+++ 03_Tris/03_Fusion.cpp
@@ -1,30 +1,64 @@
 //---------------------------------------------------------
-// Fichier        : 03_Pointeurs.cpp
+// Fichier        : 03_Fusion.cpp
 // Version        : 01 - 2022-03-15
 // Auteur(s)      : BREGUET Guy-Michel
-// But            : démontrer l'utilisation basic de pointeurs
+// But            : démontrer l'algorithm de fusion (sans tri)
 // Modifications  :
-// Remarque(s)    :
+// Remarque(s)    : une version compacte mais qui ne crée
+//                : pas de vecteur est disponible sur
+//                : https://www.cplusplus.com/reference/algorithm/merge
 //---------------------------------------------------------
 
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 
 using namespace std;
 
-//------------------------------------------------------
-unsigned PGCD(unsigned a, unsigned b) {
+//---------------------------------------------------------
+template <typename T>
+ostream& operator<< (ostream& os, const vector<T> v) {
+   os << "[";
+   for (auto it=v.begin(); it!=v.end(); ++it) {
+      if (it!=v.begin())
+         os << ", ";
+      os << *it;
+   }
+   os << "]";
+   return os;
+}
 
-   // nbre d'appels
-   static unsigned appels = 0;
-   cout << ++appels << ") " << " a=" << a << " b=" << b << endl;
+//---------------------------------------------------------
+template <typename T>
+vector<T> fusionner (const vector<T> lhs, const vector<T> rhs) {
 
-   // cas trivial
-   if (b == 0)
-      return a;
+   // cas particuliers => gain de temps
+   if (lhs.empty())
+      return rhs;
 
-   // appel récursif
-   return PGCD(b, a%b);
+   if (rhs.empty())
+      return lhs;
+
+   // vecteur de résultat (la taille est prête => gain de temps)
+   vector<T> resultat(lhs.size() + rhs.size());
+
+   // iterateurs de travail
+   auto lhs_it = lhs.begin();
+   auto rhs_it = rhs.begin();
+   auto res_it = resultat.begin();
+
+   // copie alternative
+   while (lhs_it != lhs.end() and rhs_it != rhs.end())
+      *res_it++ = *lhs_it < *rhs_it ? *lhs_it++ : *rhs_it++;
+
+   // copie la partie restante
+   if (lhs_it == lhs.end())
+      copy(rhs_it, rhs.end(), res_it);
+   else
+      copy(lhs_it, lhs.end(), res_it);
+
+   // retour du résultat
+   return resultat;
 }
 
 //------------------------------------------------------
@@ -32,24 +66,17 @@ unsigned PGCD(unsigned a, unsigned b) {
 //------------------------------------------------------
 int main () {
 
-   cout << "PGCD (Euclide) récursif" << endl;
+   const vector<int> A = {1, 3, 5, 7, 9};
+   const vector<int> B = {2, 4, 6, 8};
 
-   unsigned pgcd = PGCD(8, 5);
-//   unsigned pgcd = PGCD(34, 21);
+   cout << "vecteur A       : " << A << endl;
+   cout << "vecteur B       : " << B << endl;
+   cout << "fusionner(A, B) : " << fusionner(A, B);
+
    cout << endl;
-   cout << "PGCD(34, 21) = " << pgcd << endl;
-
    return EXIT_SUCCESS;
 }
 
-//      PGCD (Euclide) récursif
-//      1)  a=34 b=21
-//      2)  a=21 b=13
-//      3)  a=13 b=8
-//      4)  a=8 b=5
-//      5)  a=5 b=3
-//      6)  a=3 b=2
-//      7)  a=2 b=1
-//      8)  a=1 b=0
-//
-//      PGCD(34, 21) = 1
+//      vecteur A       : [1, 3, 5, 7, 9]
+//      vecteur B       : [2, 4, 6, 8]
+//      fusionner(A, B) : [1, 2, 3, 4, 5, 6, 7, 8, 9]
