16bb0895f15f91b3b6cd547affa9c95525fffca7
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-03-22 20:36:09 +0100

    update GMB

--- 03_Tris/03_Tri_Rapide.cpp
+++ 03_Tris/03_Tri_Rapide.cpp
@@ -1,159 +1,100 @@
 //---------------------------------------------------------
-// Fichier        : 03_Pointeurs.cpp
-// Version        : 01 - 2022-03-15
+// Fichier        : 03_Tri_Rapide.cpp
+// Version        : 01 - 2022-03-22
 // Auteur(s)      : BREGUET Guy-Michel
-// But            : démontrer l'utilisation basic de pointeurs
+// But            : démontrer l'algorithme de tri rapide
 // Modifications  :
 // Remarque(s)    :
 //---------------------------------------------------------
 
 #include <cstdlib>
 #include <iostream>
-#include <string>
+#include <algorithm>    // swap
 
 using namespace std;
 
+//---------------------------------------------------------
+template <typename T>
+void display(const T tab[], size_t taille);
+
+//---------------------------------------------------------
+template <typename T>
+size_t partition(T tab[], size_t lo, size_t hi) {
+
+   size_t i = lo;
+   size_t j = hi - 1;
+
+   while(true) {
+
+      // chercher élément qui devrait être à droite du pivot
+      while (tab[i] < tab[hi])
+         ++i;
+
+      // chercher élément qui devrait être à gauche du pivot
+      while (j > lo and tab[j] > tab[hi])
+         --j;
+
+      if (i >= j)
+         break;
+
+      swap (tab[i], tab[j]);
+   }
+
+   return i;
+}
+
+//---------------------------------------------------------
+template <typename T>
+void quickSort(T tab[], size_t taille) {
+   quickSort_rec(tab, 0, taille-1);
+}
+
+//---------------------------------------------------------
+template <typename T>
+void quickSort_rec(T tab[], size_t lo, size_t hi) {
+   size_t iPivot = partition(tab, lo, hi);
+   quickSort_rec(tab , lo,         iPivot - 1);
+   quickSort_rec(tab , iPivot + 1, hi        );
+}
 
 //---------------------------------------------------------
 int main() {
 
-   int tab[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-   int* ptr = tab;
+   {
+      int tab[] = {10, 11, 23, 44, 8, 15, 3, 9, 12, 45, 56, 45, 45};
+      const size_t TAILLE_TAB = sizeof(tab) / sizeof(int);
+   }
 
-   cout << "-------------------------------------"          << endl;
-   cout << "adresse du tableau ..."                         << endl;
-   cout << "-------------------------------------"          << endl;
-   cout << " tab                    : "   << (void*)tab     << endl;
-   cout << "&tab                    : "   << &tab           << endl;
-   cout << " ptr (contenu)          : "   <<  ptr           << endl;
-   cout << "&ptr (adresse)          : "   << &ptr           << endl;
-   cout << "*ptr (ref)              : "   << *ptr           << endl;
-   cout << endl;
+   {
+      char tab[] = {'T', 'R', 'I', 'R', 'A', 'P', 'I', 'D', 'E'};
+      const size_t TAILLE = sizeof(tab) / sizeof(char);
 
-   cout << "-------------------------------------"          << endl;
-   cout << "élément d'un tableau ..."                       << endl;
-   cout << "-------------------------------------"          << endl;
-   cout << "&tab[2]                 : "   << &tab[2]        << endl;
-   cout << " tab+2                  : "   <<  tab+2         << endl;
-   cout << " ptr+2                  : "   <<  ptr+2         << endl;
-   cout << " tab[2]                 : "   <<  tab[2]        << endl;
-   cout << " ptr[2]                 : "   <<  ptr[2]        << endl;
-   cout << "*(tab+2)                : "   << *(tab+2)       << endl;
+      display(tab, TAILLE);   cout << endl;
+      size_t i = partition(tab, 0, TAILLE-1);
+      display(tab, TAILLE);   cout << endl;
+      cout << "indice = " << i << endl;
+   }
 
-   cout << "*(2+tab)                : "   << *(2+tab)       << endl;
-   cout << " 2[ptr]                 : "   <<  2[ptr]        << endl;
-   cout << endl;
-
-   cout << "-------------------------------------"          << endl;
-   cout << "décallage ..."                                  << endl;
-   cout << "-------------------------------------"          << endl;
-   ptr += 2;
-   cout << "ptr += 2" << endl;
-   cout << "*(tab+2)                : "   << *(tab+2)       << endl;
-   cout << "*ptr                    : "   << *ptr           << endl;
-   cout << " ptr[0]                 : "   <<  ptr[0]        << endl;
-   cout << "*(ptr-1)                : "   << *(ptr-1)       << endl;
-   cout << " ptr[-1]                : "   <<  ptr[-1]       << endl;
-   cout << endl;
-
-   cout << "-------------------------------------"          << endl;
-   cout << "distance ..."                                   << endl;
-   cout << "-------------------------------------"          << endl;
-   cout << "&tab[6] - tab           : "   << &tab[6] - tab  << endl;
-   cout << "sizeof(tab)             : "   << sizeof(tab)    << " /!\\ "
-        << "ne fonctionne pas si tab est un ptr(param fct)" << endl;
-   cout << "sizeof(int)             : "   << sizeof(int)    << endl;
-   cout << "taille                  : "   << sizeof(tab) / sizeof(int) << endl;
-   cout << endl;
-
-   cout << "-------------------------------------"          << endl;
-   cout << "pointeur void ..."                              << endl;
-   cout << "-------------------------------------"          << endl;
-   void* ptrV = tab+2;
-   cout << "cout << *ptrV           : "   << "compile pas"  << endl;
-   cout << "cout << *(int*)ptrV     : "   << *(int*)ptrV    << endl;
-   cout << "cout << *(double*)ptrV  : "   << *(double*)ptrV << endl;
-
-   cout << "-------------------------------------"          << endl;
-   cout << "modification d'une constante ..."               << endl;
-   cout << "-------------------------------------"          << endl;
-   volatile const int CSTE = 12;    // une constante (accès en lecture seule)
-   int* ptrCste = (int*)&CSTE;      // (int*)&CSTE pour lever la contrainte
-   cout << "int* ptrCste=(int*)&CSTE  "   << endl;
-   cout << "cout << CSTE            : "   << CSTE           << endl;
-   cout << "cout << *ptrCste        : "   << *ptrCste       << endl;
-   cout << "*ptrCste = 21"                << endl;   *ptrCste = 21;  // écriture dans la constante
-   cout << "cout << *ptrCste        : "   << *ptrCste       << endl;
-   cout << "cout << CSTE            : "   << CSTE           << endl;
-
-   cout << "-------------------------------------"          << endl;
-   cout << "ne compile pas ..."                             << endl;
-   cout << "-------------------------------------"          << endl;
-   cout << "ptr *= 3       : "            << endl;    // ptr *= 3;
-   cout << "ptr += ptr     : "            << endl;    // ptr += ptr;
-   cout << endl;
 
    return EXIT_SUCCESS;
 }
 
-//      -------------------------------------
-//      adresse du tableau ...
-//      -------------------------------------
-//       tab                    : 0x7ff7bfeff270
-//      &tab                    : 0x7ff7bfeff270
-//       ptr (contenu)          : 0x7ff7bfeff270
-//      &ptr (adresse)          : 0x7ff7bfeff260
-//      *ptr (ref)              : 0
-//
-//      -------------------------------------
-//      élément d'un tableau ...
-//      -------------------------------------
-//      &tab[2]                 : 0x7ff7bfeff278
-//       tab+2                  : 0x7ff7bfeff278
-//       ptr+2                  : 0x7ff7bfeff278
-//       tab[2]                 : 2
-//       ptr[2]                 : 2
-//      *(tab+2)                : 2
-//      *(2+tab)                : 2
-//       2[ptr]                 : 2
-//
-//      -------------------------------------
-//      décallage ...
-//      -------------------------------------
-//      ptr += 2
-//      *(tab+2)                : 2
-//      *ptr                    : 2
-//       ptr[0]                 : 2
-//      *(ptr-1)                : 1
-//       ptr[-1]                : 1
-//
-//      -------------------------------------
-//      distance ...
-//      -------------------------------------
-//      &tab[6] - tab           : 6
-//      sizeof(tab)             : 40 /!\ ne fonctionne pas si tab est un ptr(param fct)
-//      sizeof(int)             : 4
-//      taille                  : 10
-//
-//      -------------------------------------
-//      pointeur void ...
-//      -------------------------------------
-//      cout << *ptrV           : compile pas
-//      cout << *(int*)ptrV     : 2
-//      cout << *(double*)ptrV  : 6.36599e-314
-//      -------------------------------------
-//      modification d'une constante ...
-//      -------------------------------------
-//      int* ptrCste=(int*)&CSTE
-//      cout << CSTE            : 12
-//      cout << *ptrCste        : 12
-//      *ptrCste = 21
-//      cout << *ptrCste        : 21
-//      cout << CSTE            : 21
-//      -------------------------------------
-//      ne compile pas ...
-//      -------------------------------------
-//      ptr *= 3       :
-//      ptr += ptr     :
-//
-//      Program ended with exit code: 0
+//---------------------------------------------------------
+template <typename T>
+void display(const T tab[], size_t taille) {
+   cout << "[";
+   for (size_t i=0; i<taille; ++i) {
+      if (i)
+         cout << ", ";
+      cout << tab[i];
+   }
+   cout << "]";
+}
+
+//       6 3 7 5 1 2 [4]
+//                    |
+//            3 1 [2] 4 7 [5]
+//                 |       |
+//               1 2 3     5 7 [6]
+//                              |
+//                              6 7
