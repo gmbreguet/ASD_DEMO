da7ccfe7c7dc30742f8884bd8310268ccfd0f85e
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-03-30 10:35:57 +0200

    update GMB

--- 04_Structures_Lineaires/04_Parametre_Adresse.cpp
+++ 04_Structures_Lineaires/04_Parametre_Adresse.cpp
@@ -29,10 +29,17 @@ void echanger(T& lhs, T& rhs) {
 //---------------------------------------------------------
 // echange par adresses (pointeurs)
 template <typename T>
-void echanger(T* ptrLhs, T* ptrRhs) {
-   T tmp   = *ptrLhs;
-   *ptrLhs = *ptrRhs;
-   *ptrRhs = tmp;
+void echanger(T* adrLhs, T* adrRhs) {
+   T tmp   = *adrLhs;
+   *adrLhs = *adrRhs;
+   *adrRhs = tmp;
+}
+
+//---------------------------------------------------------
+// avancer un pointeur
+template <typename T>
+void avancer(T** adrPtr) {
+   (*adrPtr)++;
 }
 
 //---------------------------------------------------------
@@ -47,13 +54,13 @@ int main() {
    cout << endl;
 
    cout << endl;
-   cout << "echanger(tab[2], tab[3])" << endl;
+   cout << "echanger(tab[2], tab[3])"  << endl;
    echanger(tab[2], tab[3]);
    afficher(tab, TAILLE);
    cout << endl;
 
    cout << endl;
-   cout << "echanger(tab+4, tab+5)" << endl;
+   cout << "echanger(tab+4, tab+5)"    << endl;
    echanger(tab+4, tab+5);
    afficher(tab, TAILLE);
    cout << endl;
@@ -61,9 +68,16 @@ int main() {
    cout << endl;
    int* ptr1 = tab;
    int* ptr2 = ptr1 + 8;
-   cout << "echanger(ptr1, ptr2)" << endl;
+   cout << "echanger(ptr1, ptr2)"      << endl;
    echanger(ptr1, ptr2);
    afficher(tab, TAILLE);
+   cout << endl;
+
+   cout << endl;
+   cout << "cout << *ptr1 : " << *ptr1 << endl;
+   cout << "avancer(&ptr1) "           << endl;
+   avancer(&ptr1);
+   cout << "cout << *ptr1 : " << *ptr1 << endl;
    cout << endl;
 
    return EXIT_SUCCESS;
