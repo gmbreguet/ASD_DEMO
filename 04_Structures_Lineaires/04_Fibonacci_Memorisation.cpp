5074642c119cb3c97660f13ca8e3224ac77001cd
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-04-12 00:40:25 +0200

    update GMB

--- 04_Structures_Lineaires/04_Fibonacci_Memorisation.cpp
+++ 04_Structures_Lineaires/04_Fibonacci_Memorisation.cpp
@@ -3,7 +3,7 @@
 // Version        : 01 - 2022-04-02
 // Auteur(s)      : BREGUET Guy-Michel
 // But            : démontrer l'algorithme de Fibonacci
-//                : en mode récursif tableau porgramming
+//                : en mode récursif tableau programming
 // Modifications  :
 // Remarque(s)    : n   : 0 1 2 3 4 5 6  7  8  9 10 
 //                  f(n): 0 1 1 2 3 5 8 13 21 34 55
