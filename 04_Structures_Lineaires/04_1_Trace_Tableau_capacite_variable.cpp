4c4e748ef3d5b2ec6e2848ffca0067968356d9b9
gmbreguet <guy-michel.breguet@heig-vd.ch>
2022-04-14 22:52:23 +0200

    update GMB

--- 04_Structures_Lineaires/04_1_Trace_Tableau_capacite_variable.cpp
+++ 04_Structures_Lineaires/04_1_Trace_Tableau_capacite_variable.cpp
@@ -51,33 +51,57 @@ private:
 void swap(Trace& lhs, Trace& rhs) noexcept   { lhs.swap(rhs); }
 
 //---------------------------------------------------------
-Trace f1(int i)            { cout << "f1 : "; return Trace(i); }
-Trace f2(Trace t)          { cout << "f2 : "; return t;        }
-Trace f3(const Trace& t)   { cout << "f3 : "; return t;        }
+template <typename T>
+ostream& operator<< (ostream& os, const vector<T>& v);
 
 //---------------------------------------------------------
 int main() {
 
-   cout << endl;
    cout << "----------------------------------------" << endl;
-   cout << "   constructeurs"                         << endl;
-   cout << "----------------------------------------" << endl;
-   cout << "vector<Trace> v0;";
-   vector<Trace> v0;
-   cout << endl;
+   cout << "vector<Trace> v0"                         << endl;
+   cout << "actions  : ";
+   vector<Trace> vect;
+   cout << endl << vect << endl << endl;
 
+   cout << "vect.reserve(2)"                          << endl;
+   cout << "actions  : ";
+   vect.reserve(2);
+   cout << endl << vect << endl << endl;
 
-   cout << "vect(3, Trace(2)) : ";
-   vector<Trace> vect(3, Trace(2));              cout << endl;
+   cout << "vect.push_back(Trace(1))"                 << endl;
+   cout << "actions  : ";
+   vect.push_back(Trace(1));
+   cout << endl << vect << endl << endl;
 
-   cout << "push_back         : ";
-   vect.push_back(trace1a);                      cout << endl;
+   cout << "vect.push_back(Trace(2))"                 << endl;
+   cout << "actions  : ";
+   vect.push_back(Trace(2));
+   cout << endl << vect << endl << endl;
 
-   cout << "emplace_back      : ";
-   vect.emplace_back(trace1a);                   cout << endl;
-   cout << endl;
+   cout << "vect.push_back(Trace(3))"                 << endl;
+   cout << "actions  : ";
+   vect.push_back(Trace(3));
+   cout << endl << vect << endl << endl;
 
+   cout << "vect.insert(vect.begin(), Trace(4))"      << endl;
+   cout << "actions  : ";
+   vect.insert(vect.begin(), Trace(4));
+   cout << endl << vect << endl << endl;
 
+   cout << "vect.insert(vect.begin()+1, Trace(5))"    << endl;
+   cout << "actions  : ";
+   vect.insert(vect.begin()+1, Trace(5));
+   cout << endl << vect << endl << endl;
+
+   cout << "vect.shrink_to_fit()"                     << endl;
+   cout << "actions  : ";
+   vect.shrink_to_fit();
+   cout << endl << vect << endl << endl;
+
+   cout << "vect.resize(4)"                           << endl;
+   cout << "actions  : ";
+   vect.resize(4);
+   cout << endl << vect << endl << endl;
 
    cout << endl;
    cout << "----------------------------------------" << endl;
@@ -86,3 +110,81 @@ int main() {
    return EXIT_SUCCESS;
 }
 
+//---------------------------------------------------------
+template <typename T>
+ostream& operator<< (ostream& os, const vector<T>& v) {
+   
+   os << "capacity : " << v.capacity() << endl;
+   os << "size     : " << v.size()     << endl;
+   os << "vecteur  : ";
+   
+   os << "[";
+   for (auto it=v.begin(); it!=v.end(); ++it) {
+      if (it!=v.begin())
+         os << ", ";
+      os << *it;
+   }
+   os << "]";
+   return os;
+}
+
+//      ----------------------------------------
+//      vector<Trace> v0
+//      actions  :
+//      capacity : 0
+//      size     : 0
+//      vecteur  : []
+//
+//      vect.reserve(2)
+//      actions  :
+//      capacity : 2
+//      size     : 0
+//      vecteur  : []
+//
+//      vect.push_back(Trace(1))
+//      actions  : Ci(1) CD(1) D(0)
+//      capacity : 2
+//      size     : 1
+//      vecteur  : [1]
+//
+//      vect.push_back(Trace(2))
+//      actions  : Ci(2) CD(2) D(0)
+//      capacity : 2
+//      size     : 2
+//      vecteur  : [1, 2]
+//
+//      vect.push_back(Trace(3))
+//      actions  : Ci(3) CD(3) CD(2) CD(1) D(0) D(0) D(0)
+//      capacity : 4
+//      size     : 3
+//      vecteur  : [1, 2, 3]
+//
+//      vect.insert(vect.begin(), Trace(4))
+//      actions  : Ci(4) CD(3) swap(2) =D(2) swap(1) =D(1) swap(4) =D(4) D(0)
+//      capacity : 4
+//      size     : 4
+//      vecteur  : [4, 1, 2, 3]
+//
+//      vect.insert(vect.begin()+1, Trace(5))
+//      actions  : Ci(5) CD(5) CD(4) CD(1) CD(2) CD(3) D(0) D(0) D(0) D(0) D(0)
+//      capacity : 8
+//      size     : 5
+//      vecteur  : [4, 5, 1, 2, 3]
+//
+//      vect.shrink_to_fit()
+//      actions  : CD(3) CD(2) CD(1) CD(5) CD(4) D(0) D(0) D(0) D(0) D(0)
+//      capacity : 5
+//      size     : 5
+//      vecteur  : [4, 5, 1, 2, 3]
+//
+//      vect.resize(4)
+//      actions  : D(3)
+//      capacity : 5
+//      size     : 4
+//      vecteur  : [4, 5, 1, 2]
+//
+//
+//      ----------------------------------------
+//         sortie de main
+//      ----------------------------------------
+//      D(2) D(1) D(5) D(4) Program ended with exit code: 0
