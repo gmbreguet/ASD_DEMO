781a0aa1cdf69185bab580a363e4ef1dc50e900c
gmbreguet <guy-michel.breguet@heig-vd.ch>
2023-03-02 18:40:15 +0100

    2023-03-02 18:40:14 - GMB

--- 01_Intro/01_Trier_Fct_Sort.cpp
+++ 01_Intro/01_Trier_Fct_Sort.cpp
@@ -1,6 +1,6 @@
 //---------------------------------------------------------
 // Fichier        : 01_Trier_Fct_Sort.cpp
-// Version        : 01 - 2023-02-28
+// Version        : 02 - 2023-03-02
 // Auteur(s)      : BREGUET Guy-Michel
 // But            : démontrer l'utilisation d'une fonction de tri générique
 //                  passée en paramètre d'une fonction générique
@@ -17,10 +17,21 @@ using namespace std;
 
 //------------------------------------------------------
 template <typename T, typename Fonction>
-vector<T> trier(vector<T> v, Fonction fct) {
-   fct(v.begin(), v.end());
-   return v;
+void trier(vector<T>& vect,
+           Fonction   fct) {
+   cout << "void trier(vector<T>& v, Fonction fct)" << endl;
+   fct(vect.begin(), vect.end());
 }
+
+//------------------------------------------------------
+template <typename T, typename Fonction>
+void trier(typename vector<T>::iterator begin,
+           typename vector<T>::iterator end,
+           Fonction                     fct) {
+   cout << "void trier(typename vector<T>::iterator begin, typename vector<T>::iterator end, Fonction fct)" << endl;
+   fct(begin, end);
+}
+
 
 //------------------------------------------------------
 template <typename T>
@@ -31,29 +42,22 @@ ostream& operator<< (ostream& os, const vector<T>& v);
 //------------------------------------------------------
 int main () {
    
-   {
-      cout << "----------------" << endl;
-      cout << "avec des entiers" << endl;
-      cout << "----------------" << endl;
-      const vector<int> v1 = {1, 4, 3, 8, 5, 9, 2};
-      cout << v1 << endl;
-      
-      vector<int> v2 = trier(v1, sort<vector<int>::iterator>);
-      cout << v2 << endl;
-   }
+   cout << "-------------------" << endl;
+   cout << "vector<T>"           << endl;
+   cout << "-------------------" << endl;
+   vector<int> v1 = {1, 4, 3, 8, 5, 9, 2};
+   cout << v1 << endl;
+   trier<int>(v1, sort<vector<int>::iterator>);
+   cout << v1 << endl << endl;
 
-   cout << endl;
    
-   {
-      cout << "----------------" << endl;
-      cout << "avec des doubles" << endl;
-      cout << "----------------" << endl;
-      const vector<double> v1 = {1.1, 4.4, 3.3, 8.8, 5.5, 9.9, 2.2};
-      cout << v1 << endl;
-      
-      vector<double> v2 = trier(v1, sort<vector<double>::iterator>);
-      cout << v2 << endl;
-   }
+   cout << "-------------------" << endl;
+   cout << "vector<T>::iterator" << endl;
+   cout << "-------------------" << endl;
+   vector<double> v2 = {1.1, 4.4, 3.3, 8.8, 5.5, 9.9, 2.2};
+   cout << v2 << endl;
+   trier<double>(v2.begin(), v2.end(), sort<vector<double>::iterator>);
+   cout << v2 << endl << endl;
 
    return EXIT_SUCCESS;
 }
@@ -71,14 +75,16 @@ ostream& operator<< (ostream& os, const vector<T>& v) {
    return os;
 }
 
-//      ----------------
-//      avec des entiers
-//      ----------------
+//      -------------------
+//      vector<T>
+//      -------------------
 //      [1, 4, 3, 8, 5, 9, 2]
+//      void trier(vector<T>& v, Fonction fct)
 //      [1, 2, 3, 4, 5, 8, 9]
 //
-//      ----------------
-//      avec des doubles
-//      ----------------
+//      -------------------
+//      vector<T>::iterator
+//      -------------------
 //      [1.1, 4.4, 3.3, 8.8, 5.5, 9.9, 2.2]
+//      void trier(typename vector<T>::iterator begin, typename vector<T>::iterator end, Fonction fct)
 //      [1.1, 2.2, 3.3, 4.4, 5.5, 8.8, 9.9]
